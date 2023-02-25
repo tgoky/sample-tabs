9464c0c8e553726360e7b7ffddb1fd4f23308a14
tgoky <tgokyicloud6@gmail.com>
2023-02-25 20:06:34 +0100

    reup

---
+++ navigation/tabs.js
@@ -0,0 +1,173 @@
+import React from "react";
+import {
+    Image
+} from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Home } from "../screens/";
+import { icons, COLORS } from "../constants";
+
+const Tab = createBottomTabNavigator();
+
+const tabOptions = {
+    tabBarShowLabel: false,
+    tabBarStyle: {
+        height: "10%",
+        backgroundColor: COLORS.black,
+    },
+
+}
+
+const Tabs = () => {
+    return (
+        <Tab.Navigator
+            // tabBarOptions={tabOptions}
+            screenOptions={({ route }) => ({
+                tabBarShowLabel: false,
+            tabBarStyle: [{
+                    "display": "flex",
+                    height: "10%",
+                    backgroundColor: COLORS.black,
+            },
+        null ],
+                tabBarIcon: ({ focused }) => {
+                    const tintColor = focused ? COLORS.lightGreen : COLORS.lightGray2;
+
+                    switch (route.name) {
+                        case "Home":
+                            return (
+                                <Image
+                                    source={icons.dashboard_icon}
+                                    resizeMode="contain"
+                                    style={{
+                                        tintColor: tintColor,
+                                        width: 25,
+                                        height: 25
+                                    }}
+                                />
+                            )
+
+                        case "Search":
+                            return (
+                                <Image
+                                    source={icons.portfolio_icon}
+                                    resizeMode="contain"
+                                    style={{
+                                        tintColor: tintColor,
+                                        width: 25,
+                                        height: 25
+                                    }}
+                                />
+                            )
+
+                        case "Notification":
+                            return (
+                                <Image
+                                    source={icons.trade_icon}
+                                    resizeMode="contain"
+                                    style={{
+                                        tintColor: tintColor,
+                                        width: 25,
+                                        height: 25
+                                    }}
+                                />
+                            )
+                            case "Chart":
+                                return (
+                                    <Image
+                                        source={icons.chart_icon}
+                                        resizeMode="contain"
+                                        style={{
+                                            tintColor: tintColor,
+                                            width: 25,
+                                            height: 25
+                                        }}
+                                    />
+                                )
+
+                        case "Setting":
+                            return (
+                                <Image
+                                    source={icons.menu_icon}
+                                    resizeMode="contain"
+                                    style={{
+                                        tintColor: tintColor,
+                                        width: 25,
+                                        height: 25
+                                    }}
+                                />
+                            )
+                    }
+                }
+            })}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    headerStyle: {
+                      backgroundColor: "#25282F",
+                    },
+                    headerTintColor: 'black',
+                    headerTitleStyle: {
+                      fontWeight: 'bold',
+                    },
+                  }}
+                
+            />
+            <Tab.Screen
+                name="Search"
+                component={Home}
+                options={{
+                    headerStyle: {
+                      backgroundColor: "#25282F",
+                    },
+                    headerTintColor: 'black',
+                    headerTitleStyle: {
+                      fontWeight: 'bold',
+                    },
+                  }}
+            />
+            <Tab.Screen
+                name="Notification"
+                component={Home}
+                options={{
+                    headerStyle: {
+                      backgroundColor: "#25282F",
+                    },
+                    headerTintColor: 'black',
+                    headerTitleStyle: {
+                      fontWeight: 'bold',
+                    },
+                  }}
+            />
+            <Tab.Screen
+                name="Chart"
+                component={Home}
+                options={{
+                    headerStyle: {
+                      backgroundColor: "#25282F",
+                    },
+                    headerTintColor: 'black',
+                    headerTitleStyle: {
+                      fontWeight: 'bold',
+                    },
+                  }}
+            />
+            <Tab.Screen
+                name="Setting"
+                component={Home}
+                options={{
+                    headerStyle: {
+                      backgroundColor: "#25282F",
+                    },
+                    headerTintColor: 'black',
+                    headerTitleStyle: {
+                      fontWeight: 'bold',
+                    },
+                  }}
+            />
+        </Tab.Navigator>
+    )
+}
+
+export default Tabs;
---
+++ screens/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+    SafeAreaView,
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+    FlatList
+} from 'react-native';
+
+import { COLORS, FONTS, SIZES, icons, images } from '../constants';
+
+const LineDivider = () => {
+    return (
+        <View style={{ width: 1, paddingVertical: 18 }}>
+            <View style={{ flex: 1, borderLeftColor: COLORS.lightGray, borderLeftWidth: 1 }}></View>
+        </View>
+    )
+}
+
+const Home = ({ navigation }) => {
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.black }}>
+          
+        </SafeAreaView>
+    )
+    }
+
+   
+
+
+export default Home;
